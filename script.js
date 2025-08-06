4778f31eef873eee470fab264db14766830559d1
Himank Gupta <142075465+himankgupta1@users.noreply.github.com>
2025-08-06 14:19:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-import React from 'react';
-import HelloWorld from './components/HelloWorld';
-import HelloMessage from './components/HelloMessage';
-import Counter from './components/Counter';
-import Alert from './components/Alert';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-return (
-<div style={{ margin: '0 100px' }}>
-<h3>UseCase 1 - Components, Props and States</h3>
+export default function Counter() {
+  const [seconds, setSeconds] = useState(0);   // ✅ CORRECT
 
-<HelloWorld />
+  useEffect(() => {
+    const interval = setInterval(() => setSeconds(s => s + 1), 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-{/* Single Prop */}
-<HelloMessage />
-
-{/* Multiple Props */}
-<HelloMessage name="Euler" message="Hi, Hello" />
-<HelloMessage name="Ramanujam" message="I got this in my dreams" />
-
-<Counter />
-<Alert />
-</div>
-);
+  return <p>Time Elapsed: {seconds}</p>;
 }
-
-export default App;
